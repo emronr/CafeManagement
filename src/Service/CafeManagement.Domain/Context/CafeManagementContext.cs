69e82377aafb46a2587692aa183034484fc647d4
emronr <emre998@hotmail.com>
2023-05-31 21:58:55 +0300

    DbContext was created

---
+++ src/Service/CafeManagement.Domain/Context/CafeManagementContext.cs
@@ -0,0 +1,20 @@
+﻿using CafeManagement.Domain.Entities.Cafe;
+using CafeManagement.Domain.Entities.Menu;
+using CafeManagement.Domain.Entities.Order;
+using Microsoft.EntityFrameworkCore;
+
+namespace CafeManagement.Domain.Context
+{
+    public class CafeManagementContext : DbContext
+    {
+        public CafeManagementContext(DbContextOptions<CafeManagementContext> options) : base(options)
+        {
+            
+        }
+        public virtual DbSet<Table> Tables { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+        public virtual DbSet<OrderDetail> OrderDetails { get; set; }
+    }
+}
---
+++ src/Service/CafeManagement.Domain/ServiceRegistration.cs
@@ -0,0 +1,15 @@
+using CafeManagement.Domain.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CafeManagement.Domain;
+
+public static class ServiceRegistration
+{
+    public static void AddDomainRegistrations(this ServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<CafeManagementContext>(options
+            => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+    }
+}
